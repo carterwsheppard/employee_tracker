4b4ab176960b3b466dec92fa4d6129131845326f
carterwsheppard <carterwsheppard@gmail.com>
2022-07-06 10:33:04 -0400

    progress save - DB section good to go now - need to set up routes next

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES
+('Engineering')
+,('Sales')
+,('HR')
+,('IT');
+
+
+INSERT INTO employment_role (title, salary, department_id)
+VALUES
+('Engineering Manager','120,000',1),
+('Sales Rep','70,000',2),
+('HR Specialist','70,000',3),
+('IT Help Desk','80,000',4),
+('Engineer','90,000',1);
